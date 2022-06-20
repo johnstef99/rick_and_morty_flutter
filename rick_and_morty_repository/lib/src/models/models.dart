23e3f779650fe82230b32f05809b97762ba9b198
johnstef99 <john@johnstef.com>
2022-06-20 21:21:27 +0300

    create extensions to get episode ids out from Character

--- rick_and_morty_repository/lib/src/models/models.dart
+++ rick_and_morty_repository/lib/src/models/models.dart
@@ -2,4 +2,28 @@
 // different, but still exporting them so the app will not use models directly
 // from the API
 export 'package:rick_and_morty_api/rick_and_morty_api.dart'
-    show Character, CharacterLocation, CharacterGender, CharacterStatus;
+    show
+        Character,
+        CharacterLocation,
+        CharacterGender,
+        CharacterStatus,
+        Episode;
+
+import 'package:rick_and_morty_api/rick_and_morty_api.dart' show Character;
+
+extension CharacterX on Character {
+  /// Get ids from episodes urls
+  List<int> get episodesId => episode.map((e) => parseEpisodeUrl(e)).toList()
+    ..removeWhere((e) => e == -1);
+}
+
+extension ListCharacterX on List<Character> {
+  /// Get ids from episodes urls
+  List<int> get firstEpisodeId =>
+      map((char) => parseEpisodeUrl(char.episode.first)).toList()
+        ..removeWhere((e) => e == -1);
+}
+
+/// return -1 if episodeUrl couldn't be parsed
+int parseEpisodeUrl(String episodeUrl) =>
+    int.tryParse(episodeUrl.split('/').last) ?? -1;
